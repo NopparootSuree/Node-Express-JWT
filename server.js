703d28a0a52e9dc297af615df8dbd384ff7283bb
nopparootSuree <nopparoot.su@gmail.com>
2023-06-11 20:48:20 +0700

    config server

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-const { InitDatabase } = require('./src/config/dataabse')
-
-testConnection();
 
 const app = express();
 
@@ -11,6 +8,9 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
 
+const employeeRoute = require('./src/routers/employee')
+
+app.use('/api', employeeRoute)
+
 const port = 3000
-InitDatabase()
 app.listen(port, () => console.log(`Start Server Port: ${port}`));
