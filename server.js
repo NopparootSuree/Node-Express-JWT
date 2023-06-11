66f8af6cf4bad5a335df276c655ed11c9da3083d
nopparootSuree <nopparoot.su@gmail.com>
2023-06-11 15:27:09 +0700

    update

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const { InitDatabase } = require('./src/config/dataabse')
+
+testConnection();
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+const port = 3000
+InitDatabase()
+app.listen(port, () => console.log(`Start Server Port: ${port}`));
