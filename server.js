9db33baa86aeb31ef602db4bdee127e9cf024bb8
nopparootSuree <nopparoot.su@gmail.com>
2023-06-11 22:06:56 +0700

    update knex

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const mysql = require('mysql2')
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
+
 
 const employeeRoute = require('./src/routers/employee')
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,12 @@
 require('dotenv').config();
 
 module.exports = require('knex')({
-    client: process.env.DB_CLIENT,
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT
-    },
-    migrations: {
-      directory: './database/migrations',
-    },
-    seeds: {
-      directory: './database/seeds',
-    },
-});
+      client: process.env.DB_CLIENT,
+      connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
+      }
+  });
