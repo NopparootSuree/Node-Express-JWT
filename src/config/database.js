cc28af993eb0c83ca8aaf4c460096d452f1fb529
nopparootSuree <nopparoot.su@gmail.com>
2023-06-11 20:48:36 +0700

    create database with knex

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,18 @@
-var mysql = require('mysql2');
 require('dotenv').config();
 
-exports.InitDatabase = () => {
-var conn = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-  });
-  
-conn.connect(function(err) {
-  if (err) throw err;
-  console.log("Database is Connected!");
+module.exports = require('knex')({
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
 });
-}
