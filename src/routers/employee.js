6901843fd2299629fccea1f1debfa6cb8b61b2df
nopparootSuree <nopparoot.su@gmail.com>
2023-06-14 10:59:51 +0700

    update router api table employee

--- src/routers/employee.js
+++ src/routers/employee.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { validateEmployees, createEmployees } = require('../controllers/employees')
+const employee = require('../controllers/employees')
+const middleware = require('../middlewares/employee') 
 
-router.post('/employee', validateEmployees, createEmployees)
+router.post('/employee', middleware.validateCreateEmployees, employee.createEmployees)
+router.get('/employee', employee.listEmployee)
+router.delete('/employee/:emp_no', employee.deleteEmployee)
+router.put('/employee/:emp_no', middleware.validateUpdateEmployees, employee.updateEmployee)
 
 module.exports = router;
